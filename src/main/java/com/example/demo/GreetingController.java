d481abeda503e9dacbcb9751aed357877a20c3cb
zisisp <zisis.pontikas@eurodyn.com>
2018-09-27 16:16:31 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GreetingController {
+
+  private final GreetingService greetingService;
+
+  public GreetingController(GreetingService greetingService) {
+    this.greetingService = greetingService;
+  }
+
+
+  @RequestMapping("/greeting")
+  public @ResponseBody String greeting() {
+    return greetingService.greet();
+  }
+
+}
